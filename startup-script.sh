ed8464e98a9ec011c9c2d8afeea518e1aec02332
JC Snider <jcsnider@users.noreply.github.com>
2018-10-09 16:59:59 -0400

    Startup script updates

--- startup-script.sh
+++ startup-script.sh
@@ -3,13 +3,14 @@ port=${@:$#}
 second=""
 str=${str// $port/$second}
 pdb=${str//.exe/.pdb}
+mono --version
 rm -rf *.mdb
 if [ -f "$pdb" ]; then
    echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
    pdb2mdb "$str"
    sleep 5
-   mono --debug "$str" $port nohalt
+   mono --debug "$str" $port nohalt noupnp alpine
 else
    echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
-   mono "$str" $port nohalt
+   mono "$str" $port nohalt noupnp alpine
 fi
