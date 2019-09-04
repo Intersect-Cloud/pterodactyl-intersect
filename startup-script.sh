243597835d3a2e73694b3cca4efe5e822bd3e312
JC Snider <jcsnider@users.noreply.github.com>
2019-09-04 19:49:29 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -11,7 +11,7 @@ if [ -f "$pdb" ]; then
    echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
    pdb2mdb "$str"
    sleep 5
-   mono --debug "$Intersect Server.exe" $params
+   mono --debug "Intersect Server.exe" $params
 else
    echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
    mono "Intersect Server.exe" $params
