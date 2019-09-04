5317db26bcbf4a4bac26609f3cc8fd7c8ddf9357
JC Snider <jcsnider@users.noreply.github.com>
2019-09-04 19:31:35 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,16 +1,22 @@
-str="$*"
+str="$1"
 port=${@:$#}
 second=""
 str=${str// $port/$second}
 pdb=${str//.exe/.pdb}
 mono --version
+params=""
+shift;
+for item in "$@" ; do
+  params="$params$item "
+done
+
 rm -rf *.mdb
 if [ -f "$pdb" ]; then
    echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
    pdb2mdb "$str"
    sleep 5
-   mono --debug "$str" $port nohalt noupnp alpine
+   mono --debug "$str" $params
 else
    echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
-   mono "$str" $port nohalt noupnp alpine
+   mono "$str" $params
 fi
