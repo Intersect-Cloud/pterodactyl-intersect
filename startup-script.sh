7c02f83fb2c1c29e9149d5a5c61195b56c6e8752
JC Snider <jcsnider@users.noreply.github.com>
2021-01-21 10:49:28 -0500

    Fix esqlite3 error on server launch
    By explicitly telling Mono that the working directory is a directory that the .so file will be exported to.

--- startup-script.sh
+++ startup-script.sh
@@ -11,8 +11,8 @@ if [ -f "$pdb" ]; then
    echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
    pdb2mdb "Intersect Server.exe"
    sleep 5
-   mono --debug "Intersect Server.exe" $params
+   LD_LIBRARY_PATH=. mono --debug "Intersect Server.exe" $params
 else
    echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
-   mono "Intersect Server.exe" $params
+   LD_LIBRARY_PATH=. mono "Intersect Server.exe" $params
 fi
