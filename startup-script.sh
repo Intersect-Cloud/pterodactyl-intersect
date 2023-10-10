c7e1dadf8962dbb98a87a2a6657b609df079cc06
JC Snider <jcsnider@users.noreply.github.com>
2023-10-10 00:14:53 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,18 +1,23 @@
-pdb="Intersect Server.pdb"
-mono --version | head -1
 params=""
 
 for item in "$@" ; do
   params="$params$item "
 done
 
-rm -rf *.mdb
-if [ -f "$pdb" ]; then
-   echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
-   pdb2mdb "Intersect Server.exe"
-   sleep 5
-   LD_LIBRARY_PATH=. mono --debug "Intersect Server.exe" $params
+if [ -f "Intersect Server" ]; then
+      chmod +x "Intersect Server"
+      ./"Intersect Server" $params
 else
-   echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
-   LD_LIBRARY_PATH=. mono "Intersect Server.exe" $params
+   pdb="Intersect Server.pdb"
+   mono --version | head -1
+   rm -rf *.mdb
+   if [ -f "$pdb" ]; then
+      echo "Debug file exists, creating Mono variant, error logs will contain line numbers"
+      pdb2mdb "Intersect Server.exe"
+      sleep 5
+      LD_LIBRARY_PATH=. mono --debug "Intersect Server.exe" $params
+   else
+      echo "Debug file (.pdb) not provided, error logs will not contain line numbers."
+      LD_LIBRARY_PATH=. mono "Intersect Server.exe" $params
+   fi
 fi
